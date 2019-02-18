782bfd7e6ba5f56b04e7158c7c6b1f9a15ff6150
drngsl <drngsl@qq.com>
2019-02-18 15:35:11 +0800

    init eureka-server

---
+++ eureka-server/docker/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cur=$(cd $(dirname $0);pwd)
+
+pkg_name=eureka-server
+mvn package -f $cur/../pom.xml
+
+cp $cur/../target/eureka-server*.jar $cur/
+
+pkg_ver=`ls eureka-server*  | sed "s/eureka-server-\(.*\).jar/\1/g"`
+
+docker build -t dengshaolin/eureka $cur
+
+rm -rf $cur/eureka-server*.jar
